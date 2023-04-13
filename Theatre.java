a681f48757a34f08a416e0047f617bbe306a2e9e
ybr1396 <ybr1303@gmail.com>
2023-04-13 14:37:12 +0530

    update

---
+++ Theatre.java
@@ -0,0 +1,17 @@
+class Theatre{
+
+public static void main(String[] args){
+
+Theatre rohini_theatre = new Theatre();
+rohini_theatre.show(196,4);
+
+}
+
+void show(int ticket_price,int no_of_persons)
+{
+//int ticket_price = 196;
+//int no_of_persons = 4;
+System.out.println(ticket_price*no_of_persons);
+}
+
+}
